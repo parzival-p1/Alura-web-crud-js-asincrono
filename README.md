02d005c2808249224be5e047e455aced503010db
Paco Hdez <francj.hdez@gmail.com>
2023-04-13 12:35:40 -0600

    Fetch API: added controllers/ and modified client-service.js

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 <h1 align="centr">JS en la Web: CRUD con JavaScript asíncrono</h1>
 
+```js
 json-server --watch db.json
+```
+
 Browser sync: browser-sync start --server --file . --host --port 5000 --startPath screens/lista_cliente.html
--- service/client-service.js
+++ service/client-service.js
@@ -1,36 +1,3 @@
-const crearNuevaLinea = (nombre, email) => 
-{
-    const linea = document.createElement("tr")
-    const contenido = `
-        <td class="td" data-td>
-            ${nombre}
-        </td>
-        <td>${email}</td>
-        <td>
-            <ul class="table__button-control">
-                <li>
-                    <a
-                        href="../screens/editar_cliente.html"
-                        class="simple-button simple-button--edit"
-                    >Editar</a
-                    >
-                </li>
-                <li>
-                    <button
-                        class="simple-button simple-button--delete"
-                        type="button"
-                    >
-                        Eliminar
-                    </button>
-                </li>
-            </ul>
-        </td>
-    `;
-    linea.innerHTML = contenido;
-    return linea
-};
-
-const table = document.querySelector("[data-table]")
 
 //^ Abrir http (metodo, url)
 /**
@@ -42,14 +9,11 @@ const table = document.querySelector("[data-table]")
  * ! @http: Hyper Text Transform Protocol
  * */
 
-//* Fetch API
+//^ Fetch API
 const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json()); // da formato json a respuesta
 
+export const clientServices = {
+    listaClientes //key entrance
+};
 
-//^ response >>> data
-listaClientes().then((data) => {
-    data.forEach((perfil) => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-        table.appendChild(nuevaLinea);
-    });
-}).catch((error) => alert("Ocurrio un error"));
+
